dc8f0436c9654c7a18bb7aa2f804cd9926202477
Mauricio Cardozo <you@example.com>
2022-10-18 22:09:08 -0300

    tercer  commit

--- 04-JS-IV/homework/pruebas.js
+++ 04-JS-IV/homework/pruebas.js
@@ -1,46 +1,9 @@
-const usuario = {
-    name: "mauri",
-    ape : "cardozo",
-    password : "123",
-    posts :  [
-        {
-          id: '1',
-          title: 'Aventuras en JS!',
-          likes: 10
-        },
-        {
-          id: '2',
-          title: 'Soy Henry!',
-          likes: 100
-        },
-        {
-          id: '3',
-          title: 'Qué es un JavaScript?',
-          likes: 35
-        },
-        {
-          id: '4',
-          title: 'JS Objects for Dummies',
-          likes: 42
-        },
-        {
-          id: '5',
-          title: 'Educación online',
-          likes: 99
-        },
-      ]
-    
-};
-var suma = 0;
-
-const producto = {
-    precio : 100 ,
-    descuento : 0.10,
-};
-
-
-producto.calcula = function(){
-    return this.precio - (producto.precio * producto.descuento)
-};
-
-console.log(producto, producto.calcula())
+String.prototype.invertir = function(){
+    var arr1 = this.split("");
+    var arr2 = arr1.reverse();
+    var arr3 = arr2.join("");
+ 
+    return arr3
+}
+var a = "Hola";
+console.log(a.invertir())
